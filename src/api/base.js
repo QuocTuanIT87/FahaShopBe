efa26c6b05e336c974d385220d7c6452a727202e
QuocTuanIT87 <121918629+QuocTuanIT87@users.noreply.github.com>
2023-10-27 11:47:38 +0700

    Update base.js

--- src/api/base.js
+++ src/api/base.js
@@ -2,7 +2,6 @@
 const config = {
     // PUBLIC_IMAGE_URL: 'https://bookstore-api-service.onrender.com/image/',
     PUBLIC_IMAGE_URL: `${process.env.REACT_APP_BASE_URL}/image/`,
-    
 };
 
 export default config;
