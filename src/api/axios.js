dd21d460fd70b3dfa6893398fb9744d29c41445b
QuocTuanIT87 <121918629+QuocTuanIT87@users.noreply.github.com>
2023-10-27 09:34:26 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import 'dotenv/config';
+
 export default axios.create({
     // baseURL: 'https://bookstore-api-service.onrender.com/api',
     baseURL: process.env.BASE_URL + '/api',
